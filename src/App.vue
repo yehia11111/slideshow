db87dbeca913fedeff0e58c5b59044a3abc067b6
yehia11111 <yehiaemam2006@gmail.com>
2024-06-13 15:04:51 +0100

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <div class="slideshow">
+    <div v-if="showVideo" class="video-container">
+      <video @ended="onVideoEnd" autoplay muted>
+        <source src="./assets/intro-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div v-else class="slideshow">
       <div
         v-for="(image, index) in images"
         :key="index"
@@ -26,16 +32,23 @@ export default {
     return {
       images: [img1, img2, img3],
       activeIndex: 0,
+      showVideo: true,
     };
   },
   mounted() {
-    this.startSlideshow();
+    if (!this.showVideo) {
+      this.startSlideshow();
+    }
   },
   methods: {
     startSlideshow() {
       setInterval(() => {
         this.activeIndex = (this.activeIndex + 1) % this.images.length;
       }, 10000);
+    },
+    onVideoEnd() {
+      this.showVideo = false;
+      this.startSlideshow();
     },
   },
 };
@@ -59,10 +72,10 @@ export default {
 .logo img {
   width: 200px;
   height: auto;
-  opacity: 0.8; /* Make the logo slightly transparent */
+  opacity: 0.8;
 }
 
-.slideshow {
+.slideshow, .video-container {
   width: 100%;
   height: 100%;
   position: absolute;
@@ -85,6 +98,12 @@ export default {
   opacity: 1;
 }
 
+.video-container video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
 .text {
   position: absolute;
   bottom: 10px;
@@ -94,5 +113,107 @@ export default {
   z-index: 10;
   background: rgba(255, 255, 255, 0.5);
   padding: 5px;
+}
+
+@media (min-width: 600px) {
+  .logo {
+    top: 30px;
+    left: 50px;
+  }
+
+  .logo img {
+    width: 300px;
+  }
+
+  .text {
+    font-size: 44px;
+    bottom: 10px;
+    left: 10px;
+  }
+}
+
+@media (min-width: 400px) and (max-width: 599.99px) {
+  .logo {
+    top: 20px;
+    left: 20px;
+  }
+
+  .logo img {
+    width: 150px;
+  }
+
+  .text {
+    font-size: 20px;
+    bottom: 8px;
+    left: 8px;
+  }
+}
+
+@media (min-width: 800px) and (max-width: 1199.99px) {
+  .logo {
+    top: 25px;
+    left: 30px;
+  }
+
+  .logo img {
+    width: 180px;
+  }
+
+  .text {
+    font-size: 22px;
+    bottom: 9px;
+    left: 9px;
+  }
+}
+
+@media (min-width: 1200px) and (max-width: 1599.99px) {
+  .logo {
+    top: 28px;
+    left: 40px;
+  }
+
+  .logo img {
+    width: 190px;
+  }
+
+  .text {
+    font-size: 23px;
+    bottom: 9px;
+    left: 9px;
+  }
+}
+
+@media (min-width: 1600px) and (max-width: 1919.99px) {
+  .logo {
+    top: 32px;
+    left: 45px;
+  }
+
+  .logo img {
+    width: 195px;
+  }
+
+  .text {
+    font-size: 23.5px;
+    bottom: 9.5px;
+    left: 9.5px;
+  }
+}
+
+@media (max-width: 399.99px) {
+  .logo {
+    top: 10px;
+    left: 10px;
+  }
+
+  .logo img {
+    width: 200px;
+  }
+
+  .text {
+    font-size: 46px;
+    bottom: 5px;
+    left: 5px;
+  }
 }
 </style>
